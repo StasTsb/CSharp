7d935eb238a2318fc00ef3455746af4d247daf8f
StasTsb <stastsb@gmail.com>
2021-11-22 22:03:40 +0300

    666

---
+++ Homework2/Homework2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// Напишите программу, проверяющую число, введенное с клавиатуры на четность
+ 
+namespace Homework2
+{
+
+    class Homework2
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите число, для проверки на четность");
+
+            int numbers = int.Parse(Console.ReadLine());
+
+            int result = (numbers/ 2);
+
+            if ()
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
