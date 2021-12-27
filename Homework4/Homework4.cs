2e434735aeb93de5b882528fe06ba29b8f4eb487
StasTsb <stastsb@gmail.com>
2021-12-27 21:38:32 +0300

    111

---
+++ Homework4/Homework4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Homework4
+    {
+        static void Main(string[] args)
+        {
+            /*Напишите программу которая подсчитает количество четных
+             * и нечетных целых чисел, в диапазоне который ввел пользователь*/
+
+            Console.WriteLine("Введите число");
+            
+            int limit = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            int even = 0;
+            int odd = 0;
+
+            while (count< limit) 
+            {
+                count++;
+                if (count % 2 == 0) 
+                {
+                    even++;         
+                
+                }
+                if (count %2 != 0)
+                {
+                    odd++;
+                }                    
+                        
+            }
+            Console.WriteLine("Количество нечетных чисел = " + even);
+            Console.WriteLine("Количество четных чисел = " + odd);
+        }
+    }
+}
