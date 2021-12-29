e529f371498fd7571e495e595905d27cdbb95791
StasTsb <stastsb@gmail.com>
2021-12-29 23:07:34 +0300

    111

---
+++ Homework5/Homework5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Homework5
+{
+    class Homework5
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int a = 1; a <=10; a++)
+                {
+                    Console.Write("#");
+
+                 
+                }
+
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
