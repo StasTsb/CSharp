0d3afb5ab1c35a2d2641b0105466b053909e2bbd
StasTsb <stastsb@gmail.com>
2022-01-04 22:08:21 +0300

    111

---
+++ Homework6/Homework6.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework6
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            bool accesAllowed;
+
+            string inputdata = Console.ReadLine();
+            string enterPassowrd = "qwer";
+
+            accesAllowed = enterPassowrd == inputdata ? true : false;
+
+            Console.WriteLine(accesAllowed);
+
+        }
+    }
+}
