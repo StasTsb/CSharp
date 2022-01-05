613ba308860ea20b65ff0a616bee155dadb7faf2
StasTsb <stastsb@gmail.com>
2022-01-05 22:01:15 +0300

    222

--- Homework6/Homework6.cs
+++ Homework6/Homework6.cs
@@ -2,19 +2,40 @@
 
 namespace Homework6
 {
-    class Program
+    class Homework6
     {
         static void Main(string[] args)
-            
-        {
-            bool accesAllowed;
 
+        {
+            /*
+             * тернарный оператор
+             */
+            bool accesAllowed;
             string inputdata = Console.ReadLine();
             string enterPassowrd = "qwer";
 
             accesAllowed = enterPassowrd == inputdata ? true : false;
 
             Console.WriteLine(accesAllowed);
+
+            /*
+             * тернарный оператор
+             */
+
+            //создаем массив (переменную)
+            int[] myArray;
+
+            //выделяем место, границу массива в 5 (начинается с 0)
+            myArray = new int[5];
+
+            //по индексу 1 присваеваем число 3 
+            myArray[1] = 3;
+
+            //выводим массив под индексом 1
+            Console.WriteLine(myArray[1]);
+
+
+
 
         }
     }
